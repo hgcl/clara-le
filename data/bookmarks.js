84ae0b597d61983a90f61f18ee0309883d4e38ea
hgcl <hey@clarale.com>
2022-04-06 22:34:25 +0200

    Add bookmarks page

---
+++ data/bookmarks.js
@@ -0,0 +1,307 @@
+export const categories = {
+  berlin: {
+    title: "Berlin",
+    anchor: "berlin",
+  },
+  blogroll: {
+    title: "Blogroll",
+    anchor: "blogroll",
+  },
+  cooking: {
+    title: "Cooking",
+    anchor: "cooking",
+  },
+  dataViz: {
+    title: "Data Visualization",
+    anchor: "data-visualization",
+  },
+  digitalGarden: {
+    title: "Digital Garden",
+    anchor: "digital-garden",
+  },
+  games: {
+    title: "Games",
+    anchor: "games",
+  },
+  map: {
+    title: "Map",
+    anchor: "map",
+  },
+  newsletter: {
+    title: "Newsletter",
+    anchor: "newsletter",
+  },
+  techieTool: {
+    title: "Techie Tool",
+    anchor: "techie-tool",
+  },
+  thinking: {
+    title: "Thinking",
+    anchor: "thinking",
+  },
+  travel: {
+    title: "Travel",
+    anchor: "travel",
+  },
+  typography: {
+    title: "Typography",
+    anchor: "typography",
+  },
+  writing: {
+    title: "Writing",
+    anchor: "writing",
+  },
+};
+
+export const bookmarksData = [
+  // >A
+  {
+    url: "https://notes.andymatuschak.org/About_these_notes",
+    title: "Andy Matuschak",
+    description: "Is it still necessary to talk about the digital garden guru?",
+    category: [categories.digitalGarden.anchor],
+    best: false,
+  },
+  // >B
+  {
+    url: "https://beautifulwebtype.com/",
+    title: "Beautiful Web Type",
+    description: "A curated collection of open-source typefaces and pairings",
+    category: [categories.typography.anchor],
+    best: false,
+  },
+  {
+    url: "https://berlinfoodstories.com/",
+    title: "Berlin Food Stories",
+    description:
+      "Per shares amazing food and restaurants recommendations in Berlin.",
+    category: [categories.berlin.anchor],
+    best: false,
+  },
+  // >C
+  {
+    url: "https://www.horsepaste.com/",
+    title: "Codenames",
+    description:
+      "I love codenames and have the actual game at home. This online version works great for remote play.",
+    category: [categories.games.anchor],
+    best: false,
+  },
+  {
+    url: "https://www.youtube.com/c/CuisineAndChill/",
+    title: "Cuisine And Chill (in French)",
+    description:
+      "My number one cooking channel on Youtube—I have watched all of his videos and ask for more.",
+    category: [categories.cooking.anchor],
+    best: true,
+  },
+  // >D
+  {
+    url: "https://darkreader.org/",
+    title: "Dark Reader",
+    description:
+      "Browser extension to enable dark mode on websites not supporting dark mode—this saved my eyes so many times",
+    category: [categories.techieTool.anchor],
+    best: true,
+  },
+  {
+    url: "https://www.densediscovery.com/",
+    title: "Dense Discovery",
+    description:
+      "Kai's “curation newsletter” about design, productivity and society—love the personal intros and Worthy Five sections",
+    category: [categories.newsletter.anchor],
+    best: false,
+  },
+  // >E
+  {
+    url: "https://www.eaudepoisson.com/",
+    title: "eau de poisson (in French)",
+    description: "Yep, sometimes Robin and I travel blog when we feel like it.",
+    category: [categories.travel.anchor],
+    best: false,
+  },
+  // >F
+  {
+    url: "http://feltron.com/",
+    title: "Feltron annual reports",
+    description:
+      "Annual reports by designer Nicholas Felton (from 2005 to 2014), personalized data visualization at its finest",
+    category: [categories.dataViz.anchor],
+    best: false,
+  },
+  {
+    url: "https://fontpair.co/",
+    title: "FontPair",
+    description: "Collection of Google fonts pairings",
+    category: [categories.typography.anchor],
+    best: false,
+  },
+  {
+    url: "https://fontsinuse.com/",
+    title: "Fonts in Use",
+    description: "A collaborative archive of typefaces used in real life",
+    category: [categories.typography.anchor],
+    best: false,
+  },
+  // >G
+  {
+    url: "https://garticphone.com/",
+    title: "Gartic Phone",
+    description:
+      "Playing online is not just for gamers. Gartic Phone is the love child of Telephone and Pictionary—super fun and accessible!",
+    category: [categories.games.anchor],
+    best: false,
+  },
+  // >H
+  {
+    url: "http://www.hemingwayapp.com/",
+    title: "Hemingway Editor",
+    description: "Online editor for simpler, bolder and clearer writing",
+    category: [categories.writing.anchor],
+    best: false,
+  },
+  {
+    url: "https://dev.w3.org/html5/html-author/charref",
+    title: "HTML Character Reference Chart",
+    description:
+      "Ever wondered how to write a character in HTML? Here is a full list of them that I just can't stop looking at.",
+    category: [categories.techieTool.anchor],
+    best: false,
+  },
+  {
+    url: "https://100r.co/",
+    title: "Hundred Rabbits",
+    description:
+      "When I think about inspiring people, I think about Rekka and Devine, the creators of Hundred Rabbits. They live off the grid on a sailboat, have a plant-based cooking blog, create open-source projects... Do I need to go on?",
+    category: [categories.travel.anchor, categories.blogroll.anchor],
+    best: false,
+  },
+  {
+    url: "https://hypothes.is/",
+    title: "Hypothesis",
+    description:
+      "Highlight and annotate articles online—super cool open-source project.",
+    category: [categories.techieTool.anchor],
+    best: true,
+  },
+  // >I
+  {
+    url: "https://ifdb.org/",
+    title: "Interactive Fiction Database",
+    description: "The home of all text adventures games!",
+    category: [categories.games.anchor],
+    best: false,
+  },
+  // >J
+  // >K
+  // >L
+  {
+    url: "https://languagetool.org/",
+    title: "LanguageTool",
+    description:
+      "A multilingual browser extension (and more) for grammar, style and spell checking—and it's open source",
+    category: [categories.writing.anchor, categories.techieTool.anchor],
+    best: false,
+  },
+  {
+    url: "https://www.overdrive.com/apps/libby",
+    title: "Libby App",
+    description:
+      "A library reading—but in my case mostly listening—app. Their interface is great and makes borrowing audiobooks a breeze. Check if they partnered with your local library!",
+    category: [categories.techieTool.anchor],
+    best: false,
+  },
+  // >M
+  {
+    url: "https://www.youtube.com/user/Maangchi",
+    title: "Maangchi",
+    description:
+      "If you are into Korean food, you certainly know Maangchi—the Korean mama that you always wished you had.",
+    category: [categories.cooking.anchor],
+    best: false,
+  },
+  {
+    url: "https://maggieappleton.com/garden/",
+    title: "Maggie Appleton",
+    description: "Maggie Appleton's illustration-based digital garden",
+    category: [categories.digitalGarden.anchor],
+    best: false,
+  },
+  {
+    url: "https://mindmeld.url/",
+    title: "Mindmeld",
+    description:
+      "Wavelength is a social party game that fosters conversations. Mindmeld is its free online spin-off.",
+    category: [categories.games.anchor],
+    best: false,
+  },
+  {
+    url: "https://www.youtube.com/c/mynameisandong/",
+    title: "My Name Is Andong",
+    description:
+      "One of my favorite cooking channels on Youtube—informative, funny and dynamic",
+    category: [categories.cooking.anchor],
+    best: false,
+  },
+  // >N
+  // >O
+  {
+    url: "https://omglord.com/",
+    title: "OMGLORD",
+    description:
+      "Designer Gabby Lord shares amazing resources on her website and through her newsletter.",
+    category: [categories.blogroll.anchor],
+    best: false,
+  },
+  // >P
+  {
+    url: "https://practicaltypography.com/",
+    title: "Practical Typography",
+    description:
+      "It's not really a website, it's not really a book. One thing is certain: it is a treasure chest of typographic knowledge.",
+    category: [categories.typography.anchor],
+    best: false,
+  },
+  {
+    url: "https://pudding.cool/",
+    title: "The Pudding",
+    description:
+      'In their own words, The Pudding "is a digital publication that explains ideas debated in culture with visual essays."',
+    category: [categories.dataViz.anchor],
+    best: false,
+  },
+  // >Q
+  // >R
+  // >S
+  // >T
+  {
+    url: "https://www.theydrawandtravel.com/",
+    title: "They draw and travel",
+    description:
+      "Collection of illustrated travel map created by artists from around the world",
+    category: [categories.map.anchor],
+    best: false,
+  },
+  {
+    url: "https://type-scale.com/",
+    title: "Type scale",
+    description:
+      "My go-to tool whenever I need to create a new website. It just makes font size choices so much more straightforward.",
+    category: [categories.techieTool.anchor],
+    best: false,
+  },
+  // >U
+  // >V
+  // >W
+  {
+    url: "https://waitbutwhy.com/",
+    title: "Wait But Why",
+    description:
+      "Tim Urban is one of the most thoughtful thinkers on the internet. He brings complex and fascinating topics within everyone's reach through (very) long form blog posts.",
+    category: [categories.thinking.anchor],
+    best: true,
+  },
+  // >X
+  // >Y
+  // >Z
+];
---
+++ scripts/buildBookmarksPage.js
@@ -0,0 +1,20 @@
+import { categories, bookmarksData } from "../data/bookmarks.js";
+
+export function buildPage(html) {
+  const postsHtml = Object.values(categories)
+    .map(
+      (cat) =>
+        `<h2 id=${cat.anchor}>${cat.title}</h2>` +
+        `<ul>` +
+        bookmarksData
+          .filter((item) => item.category.includes(cat.anchor))
+          .map(
+            (item) =>
+              `<li><a href="${item.url}">${item.title}</a>&emsp;<span style="opacity: var(--alt-text-opacity);">${item.description}</span></li>`
+          )
+          .join("") +
+        `</ul>`
+    )
+    .join("");
+  return html.replace("<div>Data generated here</div>", postsHtml);
+}
