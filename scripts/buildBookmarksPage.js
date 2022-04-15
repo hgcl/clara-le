0ae76b8544867b1e0eae97d7bca2047c597f3b34
hgcl <hey@clarale.com>
2022-04-15 17:24:01 +0200

    Install dotenv and remove tests

--- scripts/buildBookmarksPage.js
+++ scripts/buildBookmarksPage.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 async function fetchBookmarksJSON() {
   const url = "https://api.hypothes.is/api/search?limit=200&group=qoP5v2Eg";
