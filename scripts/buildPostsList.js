3ec99b6598f648593201dce2dce559837b74bd49
hgcl <hey@clarale.com>
2022-12-15 12:56:15 +0100

    Organize posts by year

--- scripts/buildPostsList.js
+++ scripts/buildPostsList.js
@@ -1,6 +1,13 @@
 import getPosts from "./getPosts";
 
 export async function buildPage(html) {
+  // Create year array
+  const currentYear = new Date(Date.now()).getFullYear();
+  const firstYear = 2020;
+  let yearArray = [];
+  for (let i = currentYear; i >= firstYear; i--) {
+    yearArray.push(new Date(i, 1, 1).getFullYear());
+  }
   // Get headline H1
   const h1Regex = /<([h1]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)/gm;
   const headline = html.match(h1Regex);
@@ -12,26 +19,37 @@ export async function buildPage(html) {
       : "mmmh not sure what to write here";
   try {
     const posts = await getPosts(dirCategory);
-    const postsHtml = posts
-      .sort(
-        (a, b) =>
-          new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
-      )
+    const orderedPosts = posts.sort(
+      (a, b) =>
+        new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
+    );
+    const postsHtml = Object.values(yearArray)
       .map(
-        (post) =>
-          `<li class="post-row"><time class="label dt-duration" datetime="${
-            post.dateCreated
-          }">
+        (cat) =>
+          `<h2>${cat}</h2>` +
+          `<ul id="blog-list" class="nobullet">` +
+          orderedPosts
+            .filter((post) => new Date(post.dateCreated).getFullYear() == cat)
+            .map(
+              (post) =>
+                `<li class="post-row"><time class="label dt-duration" datetime="${
+                  post.dateCreated
+                }">
            ${new Date(post.dateCreated).toLocaleDateString("en-US", {
              month: "short",
              day: "numeric",
            })}
           </time><span><a href="${post.slug}">${post.title}</a>${
-            post.subtitle ? `<span class="subtle">${post.subtitle}</span>` : ""
-          }</span></li>`
+                  post.subtitle
+                    ? `<span class="subtle">${post.subtitle}</span>`
+                    : ""
+                }</span></li>`
+            )
+            .join("") +
+          `</ul>`
       )
       .join("");
-    return html.replace("<li>Posts go here</li>", postsHtml);
+    return html.replace("<p>Posts go here</p>", postsHtml);
   } catch (error) {
     throw new Error(`Failed to build page: ${error}`);
   }
