04c4108b5fc6661f4f1e06250ef274d5f757295d
Clara Le <hey@clarale.com>
2023-07-12 11:58:44 +0200

    Add links to data tags

--- scripts/formatPostDetails.js
+++ scripts/formatPostDetails.js
@@ -11,6 +11,16 @@ export function minToDurationString(mins) {
 
 export async function buildPage(html, frontmatter) {
   try {
+    function linkReplacer(match, p1, p2, p3, p4, p5, p6, offset, string) {
+      let newString = `class="${p1}-data-tags"><a href="/${p1}s/?q=${p2}">${p2}</a>`;
+      const capturingGroups = [p3, p4, p5, p6];
+      capturingGroups.forEach(function (p) {
+        if (p) {
+          newString = newString + `, <a href="/${p1}s/?q=${p}">${p}</a>`;
+        }
+      });
+      return newString;
+    }
     return (
       html
         // we include the html <> tags to avoid replacing the datetime attribute value
@@ -29,6 +39,11 @@ export async function buildPage(html, frontmatter) {
         )
         // Make sure all commas are followed by a whitespace
         .replace(/(,(?=\S))/g, ", ")
+        // Add links to data tags (max 5)
+        .replace(
+          /class\="(recipe|post)-data-tags"\>(\w+)(?:.\s)?(\w+)?(?:.\s)?(\w+)?(?:.\s)?(\w+)?(?:.\s)?(\w+)?/g,
+          linkReplacer
+        )
     );
   } catch (error) {
     throw new Error(`Failed to build page: ${error}`);
