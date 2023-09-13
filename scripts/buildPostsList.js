8c20140fc3f9c87a122964cfd199473c84a5e0e9
Clara Le <hey@clarale.com>
2023-09-13 17:00:21 +0200

    Fix post list link styles

--- scripts/buildPostsList.js
+++ scripts/buildPostsList.js
@@ -27,9 +27,9 @@ export async function buildPage(html) {
              month: "short",
              day: "numeric",
            })}
-          </time><span class="details"><a class="secondary-link" href="${
-            post.slug
-          }">${post.title}</a>${
+          </time><span class="details"><a href="${post.slug}">${
+                  post.title
+                }</a>${
                   post.subtitle
                     ? `<span class="subtle">${post.subtitle}</span>`
                     : ""
