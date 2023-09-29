ad8a70f99df1e400028ca42bcc186fc9073793ee
Clara Le <hey@clarale.com>
2023-09-29 15:11:45 +0200

    Add learning page

---
+++ scripts/buildLearningList.js
@@ -0,0 +1,35 @@
+import getPosts from "./getPosts";
+const DIR_CATEGORY = "learning";
+
+export async function buildPage(html) {
+  try {
+    const posts = await getPosts(DIR_CATEGORY, true);
+    const orderedPosts = posts.sort(
+      (a, b) =>
+        new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
+    );
+    const postsHtml = orderedPosts
+      .map(
+        (post) =>
+          `<article data-tag="${post.dataTag}">
+          <div class="post-header"><h2>${
+            post.title
+          }</h2><span class="label"><time class="dt-duration" datetime="${
+            post.dateCreated
+          }">
+          ${new Date(post.dateCreated).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+         </time> \/ ${post.dataTag}</span></div>
+          <span class="details">${
+            post.subtitle ? `<span class="subtle">${post.subtitle}</span>` : ""
+          }</span>${post.content}</article>`
+      )
+      .join("");
+    return html.replace("<p>Posts go here</p>", postsHtml);
+  } catch (error) {
+    throw new Error(`Failed to build page: ${error}`);
+  }
+}
--- scripts/getPosts.js
+++ scripts/getPosts.js
@@ -1,6 +1,12 @@
 import { load } from "js-yaml";
 import { cwd } from "process";
 import { readdir, readFile } from "fs/promises";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import { minify as minifier } from "html-minifier-terser";
 import * as path from "path";
 const PAGES_DIR = `${cwd()}/pages`;
 
@@ -14,23 +20,78 @@ function getFrontmatter(file) {
       file
     );
   if (match) {
-    return load(match[1]);
+    const frontmatter = load(match[1]);
+    const content = file.slice(match[0].length);
+    return { frontmatter, content };
   } else {
     return {};
   }
 }
 
-export default async function getPosts(dirCategory) {
+/**
+ * Parse the file into an AST, transform with unified plugins,
+ * and convert back into html.
+ */
+function processMarkdown(file) {
+  return (
+    unified()
+      /**
+       * `remark-parse` parses the markdown source to mdast.
+       * https://github.com/remarkjs/remark/tree/main/packages/remark-parse
+       */
+      .use(remarkParse)
+      /**
+       * `remark-gfm` adds support for GitHub Flavored Markdown, including
+       * tables and footnotes.
+       * https://github.com/remarkjs/remark-gfm
+       */
+      .use(remarkGfm)
+      /**
+       * `remark-rehype` transforms the mdast into hast.
+       * https://github.com/remarkjs/remark-rehype
+       */
+      .use(remarkRehype, { allowDangerousHtml: true })
+      /**
+       * `rehype-stringify` transforms the hast into HTML.
+       * https://github.com/rehypejs/rehype/tree/main/packages/rehype-stringify
+       */
+      .use(rehypeStringify, { allowDangerousHtml: true })
+      .process(file)
+      .then((vfile) => String(vfile))
+  );
+}
+
+/**
+ * Minify an HTML string using html-minifier-terser.
+ */
+async function minify(html) {
+  return minifier(html, {
+    collapseWhitespace: true,
+    removeComments: true,
+    collapseBooleanAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
+    removeOptionalTags: true,
+    minifyJS: true,
+  });
+}
+
+export default async function getPosts(dirCategory, withPostContent = false) {
   const files = await readdir(PAGES_DIR + "/" + dirCategory);
   const posts = await Promise.all(
     files
       .filter((i) => path.extname(i) !== ".json")
       .map(async (file) => {
-        const content = await readFile(
+        const fileContent = await readFile(
           `${PAGES_DIR}/${dirCategory}/${file}`,
           "utf-8"
         );
-        const frontmatter = getFrontmatter(content);
+        let html = "";
+        const { frontmatter, content } = getFrontmatter(fileContent);
+        if (withPostContent == true) {
+          html = await processMarkdown(content);
+          html = await minify(html);
+        }
         return {
           title: frontmatter.title,
           subtitle: frontmatter.subtitle,
@@ -44,6 +105,7 @@ export default async function getPosts(dirCategory) {
           slug: `/${dirCategory}/${file
             .replace(".md", "")
             .replace(".html", "")}`,
+          content: html,
         };
       })
   );
