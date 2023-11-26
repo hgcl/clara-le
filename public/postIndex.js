d547e36b94b9bfb25a947afc7e07c12257c8f58c
Clara Le <hey@clarale.com>
2023-11-26 11:35:58 +0100

    WIP search feature with placeholder content

---
+++ public/postIndex.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Moby Dick",
+    text: "Call me Ishmael. Some years ago...",
+    category: "fiction",
+  },
+  {
+    id: 2,
+    title: "Zen and the Art of Motorcycle Maintenance",
+    text: "I can see by my watch...",
+    category: "fiction",
+  },
+  {
+    id: 3,
+    title: "Neuromancer",
+    text: "The sky above the port was...",
+    category: "fiction",
+  },
+  {
+    id: 4,
+    title: "Zen and the Art of Archery",
+    text: "At first sight it must seem...",
+    category: "non-fiction",
+  },
+];
