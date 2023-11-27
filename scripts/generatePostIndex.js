be4471e57d6444b9e054719d471b3519ee87e3bc
Clara Le <hey@clarale.com>
2023-11-27 15:45:49 +0100

    First working version without full content

--- scripts/generatePostIndex.js
+++ scripts/generatePostIndex.js
@@ -3,9 +3,27 @@
  * that is used for the search feature
  */
 
-import getPosts from "./getPosts";
+import getPosts from "./getPosts.js";
+import { writeFileSync } from "fs";
+const NOTES_DIR = "posts";
+const PUBLIC_DIR = "./public/";
 
 export default async function process() {
-  const posts = await getPosts("posts", true);
-  console.log(posts);
+  try {
+    const posts = await getPosts(NOTES_DIR, true);
+    const postIndex =
+      `export default [` +
+      posts
+        .map(
+          (post, index) =>
+            `{ id: ${index}, title: "${post.title}", subtitle: "${post.subtitle}", slug: "${post.slug}", dateCreated: "${post.dateCreated}"},`
+        )
+        .join("") +
+      `];`;
+    console.log(postIndex);
+    writeFileSync(PUBLIC_DIR + "postIndex.js", postIndex, "utf8");
+    console.log("Generated postIndex.js.");
+  } catch (error) {
+    throw new Error(`Failed to generate data: ${error}`);
+  }
 }
