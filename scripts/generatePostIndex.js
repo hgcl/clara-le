9644a509a80e77dfc7549ff7eadc5a0ed3ec3b88
Clara Le <hey@clarale.com>
2023-11-27 16:44:41 +0100

    Working search for posts

--- scripts/generatePostIndex.js
+++ scripts/generatePostIndex.js
@@ -5,21 +5,33 @@
 
 import getPosts from "./getPosts.js";
 import { writeFileSync } from "fs";
-const NOTES_DIR = "posts";
+const POSTS_DIR = "posts";
 const PUBLIC_DIR = "./public/";
+
+function htmlToJson(html) {
+  return html
+    .replace(/<[^>]*>?/g, " ")
+    .replace(/["]/g, "")
+    .replace(/[\n]/g, " ");
+}
+
+function mapJson(posts) {
+  return posts
+    .map(
+      (post, index) =>
+        `{ id: ${index}, title: "${post.title}", subtitle: "${
+          post.subtitle
+        }", slug: "${post.slug}", dateCreated: "${
+          post.dateCreated
+        }",  content: "${htmlToJson(post.content)}"},`
+    )
+    .join("");
+}
 
 export default async function process() {
   try {
-    const posts = await getPosts(NOTES_DIR, true);
-    const postIndex =
-      `export default [` +
-      posts
-        .map(
-          (post, index) =>
-            `{ id: ${index}, title: "${post.title}", subtitle: "${post.subtitle}", slug: "${post.slug}", dateCreated: "${post.dateCreated}"},`
-        )
-        .join("") +
-      `];`;
+    const posts = await getPosts(POSTS_DIR, true);
+    const postIndex = `export default [` + mapJson(posts) + `];`;
     console.log(postIndex);
     writeFileSync(PUBLIC_DIR + "postIndex.js", postIndex, "utf8");
     console.log("Generated postIndex.js.");
