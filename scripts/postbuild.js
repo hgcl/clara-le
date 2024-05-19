5514bb7acb481858a0ccf74499ae5515199c250f
Clara Le <hey@clarale.com>
2024-05-19 15:49:00 +0700

    WIP: Make readdir async and hide comment section when no comment

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,15 +1,17 @@
-import { readFile, writeFile } from "fs/promises";
-import { readdirSync } from "fs";
+import { readdir, readFile, writeFile } from "fs/promises";
 
 const OUT_DIR = `./dist`;
 const POSTS_DIR = `posts`;
+const COMMENTS_DIR = `./data/comments`;
 
-function getBuiltPosts() {
+async function getBuiltPosts() {
   try {
-    const blogDirs = readdirSync(OUT_DIR + "/" + POSTS_DIR);
-    for (const blog of blogDirs) {
-      // Ignore the index.html file at the root of dist/posts
-      if (blog !== "index.html") {
+    const blogDir = await readdir(OUT_DIR + "/" + POSTS_DIR);
+    const commentDir = await readdir(COMMENTS_DIR);
+    for (const blog of blogDir) {
+      // Condition 1: Ignore the index.html file at the root of dist/posts
+      // Condition 2: Blog posts has comments
+      if (blog !== "index.html" && commentDir.includes(`posts-` + blog)) {
         injectComments(
           OUT_DIR + "/" + POSTS_DIR + "/" + blog + "/index.html",
           "<section>REPLACEMENT</section>"
@@ -25,7 +27,7 @@ async function injectComments(filePath, replacement) {
   try {
     const content = await readFile(filePath, "utf-8");
     const replaced = content.replace(
-      "<section>Comments injected here</section>",
+      "<section hidden>Comments injected here</section>",
       replacement
     );
     await writeFile(filePath, replaced);
