1dd39d9293ac364c2126a43439c1d21a958f7d48
Clara Le <hey@clarale.com>
2024-05-19 15:05:13 +0700

    WIP: Inject placeholder comment in blog posts

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,10 +1,25 @@
-import { readdir, readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+import { readdirSync } from "fs";
 
 const OUT_DIR = `./dist`;
 const POSTS_DIR = `posts`;
-const TEMP_PATH = `46-three-thousand-kilometers/index.html`;
 
-// const files = await readdir(OUT_DIR + "/" + POSTS_DIR);
+function getBuiltPosts() {
+  try {
+    const blogDirs = readdirSync(OUT_DIR + "/" + POSTS_DIR);
+    for (const blog of blogDirs) {
+      // Ignore the index.html file at the root of dist/posts
+      if (blog !== "index.html") {
+        injectComments(
+          OUT_DIR + "/" + POSTS_DIR + "/" + blog + "/index.html",
+          "<section>REPLACEMENT</section>"
+        );
+      }
+    }
+  } catch (error) {
+    throw new Error(`Failed to get blog post files: ${error}`);
+  }
+}
 
 async function injectComments(filePath, replacement) {
   try {
@@ -13,16 +28,12 @@ async function injectComments(filePath, replacement) {
       "<section>Comments injected here</section>",
       replacement
     );
-
     await writeFile(filePath, replaced);
   } catch (error) {
     throw new Error(`Failed to inject comments: ${error}`);
   }
 }
 
-injectComments(
-  OUT_DIR + "/" + POSTS_DIR + "/" + TEMP_PATH,
-  "<section>NEW TEXT HERE</section>"
-);
+getBuiltPosts();
 
 console.log("Postbuild was run.");
