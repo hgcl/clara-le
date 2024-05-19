504f4666aa70996ac393e092bde44010505dd9f1
Clara Le <hey@clarale.com>
2024-05-19 11:50:16 +0700

    WIP: Inject test text in one specific file

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,1 +1,28 @@
-console.log("hello world");
+import { readdir, readFile, writeFile } from "fs/promises";
+
+const OUT_DIR = `./dist`;
+const POSTS_DIR = `posts`;
+const TEMP_PATH = `46-three-thousand-kilometers/index.html`;
+
+// const files = await readdir(OUT_DIR + "/" + POSTS_DIR);
+
+async function injectComments(filePath, replacement) {
+  try {
+    const content = await readFile(filePath, "utf-8");
+    const replaced = content.replace(
+      "<section>Comments injected here</section>",
+      replacement
+    );
+
+    await writeFile(filePath, replaced);
+  } catch (error) {
+    throw new Error(`Failed to inject comments: ${error}`);
+  }
+}
+
+injectComments(
+  OUT_DIR + "/" + POSTS_DIR + "/" + TEMP_PATH,
+  "<section>NEW TEXT HERE</section>"
+);
+
+console.log("Postbuild was run.");
