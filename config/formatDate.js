c728e62ef8245aa5b00a5d6972d881281499f793
Clara Le <hey@clarale.com>
2024-05-30 11:06:48 +0700

    (wip) Create two different default date formats

--- config/formatDate.js
+++ config/formatDate.js
@@ -1,6 +1,16 @@
 // Format dates into YYYY-MM-DD format
+// Filter docs: https://www.11ty.dev/docs/languages/nunjucks/#filters
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("formatDate", function (date) {
-    return new Date(date).toLocaleDateString("en-CA");
+  eleventyConfig.addFilter("formatDate", function (date, size) {
+    let newDate = new Date(date);
+    if (size === "short") {
+      newDate = newDate.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+      });
+    } else {
+      newDate = newDate.toLocaleDateString("en-CA");
+    }
+    return newDate;
   });
 };
