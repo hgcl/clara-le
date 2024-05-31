207bd82d25953190f15a9a2b565d859686797983
Clara Le <hey@clarale.com>
2024-05-31 12:03:18 +0700

    (wip) Generate recipe posts
    Still under construction

---
+++ src/_data/recipes.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+// Get JSON files from recipes directory
+const recipesDir = path.resolve(__dirname, "../recipes");
+
+const recipes = fs
+  .readdirSync(recipesDir)
+  .filter((name) => path.extname(name) === ".json")
+  .map((name) => ({
+    key: path.parse(name).name,
+    ...require(path.join(recipesDir, name)),
+  }));
+
+module.exports = recipes;
