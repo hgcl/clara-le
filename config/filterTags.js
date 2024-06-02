5f8d759b1db40fc92920c064c5d47a9a920d44fe
Clara Le <hey@clarale.com>
2024-06-02 23:15:35 +0700

    (wip) Update filterTags.js filter so that it filters out all nav tags when called

---
+++ config/filterTags.js
@@ -0,0 +1,8 @@
+// Filter out all nav tags from displayed tag lists
+// Filter docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("filterTags", function (array) {
+    const wordsRemoved = ["posts", "media"];
+    return array.filter((el) => !wordsRemoved.includes(el));
+  });
+};
