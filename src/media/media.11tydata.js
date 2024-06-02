b42086f85edf9fe776ba31f2029774c08cb43a7d
Clara Le <hey@clarale.com>
2024-06-02 17:39:48 +0700

    (wip) Media page first draft

---
+++ src/media/media.11tydata.js
@@ -0,0 +1,8 @@
+// Applies to all files in this posts directory
+// Source docs: https://www.11ty.dev/docs/data-computed/
+module.exports = {
+  eleventyComputed: {
+    // Generate post creation year automatically from dateCreated frontmatter
+    yearCreated: (data) => new Date(data.dateCreated).getFullYear(),
+  },
+};
