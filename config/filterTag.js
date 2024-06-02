94f0360fd6a831eb33ceebfd9fd2dd3ccb5107eb
Clara Le <hey@clarale.com>
2024-06-02 17:43:57 +0700

    (wip) Add comment

--- config/filterTag.js
+++ config/filterTag.js
@@ -1,4 +1,6 @@
 // Filter out a specified tag from an array
+// TODO make a list of unchanging tags to exclude
+// Filter docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("filterTag", function (array, tag) {
     return array.filter((e) => e !== tag);
