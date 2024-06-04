092c4a9354a087e060f5f8d563b75f0e783b2348
Clara Le <hey@clarale.com>
2024-06-04 17:52:31 +0700

    (wip) Add learning page to nav

--- config/filterTags.js
+++ config/filterTags.js
@@ -2,7 +2,7 @@
 // Filter docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("filterTags", function (array) {
-    const wordsRemoved = ["posts", "media"];
+    const wordsRemoved = ["posts", "media", "learning"];
     return array.filter((el) => !wordsRemoved.includes(el));
   });
 };
