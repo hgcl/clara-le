defaa9e34fc443c5ec25b02c92cb57b46e2bd2ad
Clara Le <hey@clarale.com>
2024-06-14 12:14:00 +0700

    Add automatic anchors to headings support

--- _config/markdownItPlugin.js
+++ _config/markdownItPlugin.js
@@ -1,19 +1,33 @@
-// Adds markdown footnote support
-// https://www.alpower.com/tutorials/configuring-footnotes-with-eleventy
+// Adds markdown extra support
+// Footnotes: https://www.alpower.com/tutorials/configuring-footnotes-with-eleventy
+// Automatic anchor on headings: https://11ty.rocks/eleventyjs/slugs-anchors/
 
 module.exports = function (eleventyConfig) {
-  // set markdown footnote processor
-  let markdownIt = require("markdown-it");
-  let markdownItFootnote = require("markdown-it-footnote");
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const markdownItFootnote = require("markdown-it-footnote");
+  const slugify = require("slugify");
 
-  let options = {
+  let markdownItOptions = {
     html: true, // Enable HTML tags in source
     breaks: true, // Convert '\n' in paragraphs into <br>
     linkify: true, // Autoconvert URL-like text to links
   };
 
+  let markdownItAnchorOptions = {
+    level: 2, // minimum level header (anchors will only be applied to h2 level headers and below)
+    slugify: (str) =>
+      slugify(str, {
+        lower: true,
+        strict: true,
+        remove: /["]/g,
+      }),
+  };
+
   // configure the library with options
-  let markdownLib = markdownIt(options).use(markdownItFootnote);
+  let markdownLib = markdownIt(markdownItOptions)
+    .use(markdownItAnchor, markdownItAnchorOptions)
+    .use(markdownItFootnote);
   // set the library to process markdown files
   eleventyConfig.setLibrary("md", markdownLib);
 };
