e95696b097559c991724b811569464f550d9f167
Clara Le <hey@clarale.com>
2025-04-24 21:27:20 +0800

    Fix URL recognition for webmentions

--- _config/getWebmentionsForUrl.js
+++ _config/getWebmentionsForUrl.js
@@ -2,14 +2,29 @@ export default function (eleventyConfig) {
   eleventyConfig.addFilter("getWebmentionsForUrl", function (webmentions, url) {
     url = `https://clarale.com${url}`;
     const likes = ["like-of"];
-    const repost = ["repost-of"];
+    const reposts = ["repost-of"];
     const messages = ["mention-of", "in-reply-to"];
 
+    const matchingURLs = (entry) => {
+      // Add "/" to the end of URLs if missing
+      let wmTarget = entry["wm-target"];
+      if (wmTarget.slice(-1) !== "/") {
+        wmTarget = wmTarget + "/";
+      }
+      return wmTarget === url;
+    };
     const hasRequiredFields = (entry) => {
       const { author, published, content } = entry;
-      return author.name && published && content;
+      const wmReceived = entry["wm-received"];
+      return author.name && content && (published || wmReceived);
     };
-    const truncate = (entry) => {
+    const setDate = (entry) => {
+      if (!entry.published) {
+        entry.published = entry["wm-received"];
+      }
+      return entry;
+    };
+    const truncateContent = (entry) => {
       entry.content.text = entry.content.text.slice(0, 1000);
       delete entry.content.html;
       return entry;
@@ -17,26 +32,29 @@ export default function (eleventyConfig) {
 
     // Filter and map webmentions.json
     const likesMapped = webmentions
-      .filter((entry) => entry["wm-target"] === url)
-      .filter((entry) => likes.includes(entry["wm-property"]));
-    const repostMapped = webmentions
-      .filter((entry) => entry["wm-target"] === url)
-      .filter((entry) => repost.includes(entry["wm-property"]))
+      .filter(matchingURLs)
+      .filter((entry) => likes.includes(entry["wm-property"]))
+      .map(setDate);
+    const repostsMapped = webmentions
+      .filter(matchingURLs)
+      .filter((entry) => reposts.includes(entry["wm-property"]))
       .filter(hasRequiredFields)
-      .map(truncate);
+      .map(setDate)
+      .map(truncateContent);
     const messagesMapped = webmentions
-      .filter((entry) => entry["wm-target"] === url)
+      .filter(matchingURLs)
       .filter((entry) => messages.includes(entry["wm-property"]))
       .filter(hasRequiredFields)
-      .map(truncate);
+      .map(setDate)
+      .map(truncateContent);
 
     const webmentionCount =
-      likesMapped.length + repostMapped.length + messagesMapped.length;
+      likesMapped.length + repostsMapped.length + messagesMapped.length;
 
     return {
       count: webmentionCount,
       likes: likesMapped,
-      repost: repostMapped,
+      reposts: repostsMapped,
       messages: messagesMapped,
     };
   });
